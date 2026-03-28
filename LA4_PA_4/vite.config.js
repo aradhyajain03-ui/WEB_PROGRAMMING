75b85420eec88f6c9336acc02e079603f3ed667a
aradhyajain03-ui <aradhyasarafjain64@gmail.com>
2026-03-28 22:33:29 +0530

    Add files via upload

--- LA4_PA_4/vite.config.js
+++ LA4_PA_4/vite.config.js
@@ -1,2 +1,8 @@
-import { defineConfig } from 'vite';
-export default defineConfig({ root: '.', publicDir: 'public' });
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    port: 3000,
+    open: true
+  }
+})
